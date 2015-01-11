d3b1c038810a5d25d1835a78535190ffbd47d551
Rego <jay.rego.14@gmail.com>
2015-01-11 16:34:53 +0200

    This is the first back up of project PMS ;)...

---
+++ Web App/js/login.js
@@ -0,0 +1,22 @@
+$(':text').focusin(function(){
+	$(this).css('background-color', 'grey');
+});
+
+$(':password').focusin(function(){
+	$(this).css('background-color', 'grey');
+});
+
+$(':text').blur(function(){
+	$(this).css('background-color', 'white');
+});
+$(':password').blur(function(){
+	$(this).css('background-color', 'white');
+});
+
+$(':submit').click(function(){
+	$(this).attr('value', 'Logging in...');
+});
+
+$('#').click(function(){
+	$('')
+});
